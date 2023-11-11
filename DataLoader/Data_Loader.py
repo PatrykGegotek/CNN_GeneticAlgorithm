dad776e809138a6d46158f6525ba5816be7b96da
piotrekg35 <72696860+piotrekg35@users.noreply.github.com>
2023-11-11 18:40:35 +0100

    Update Data_Loader.py

--- DataLoader/Data_Loader.py
+++ DataLoader/Data_Loader.py
@@ -1,21 +1,16 @@
-from keras.preprocessing.image import ImageDataGenerator
-import os
+import tensorflow as tf
 
-def load_and_scale_images(data_dir, target_size=(64, 64), batch_size=600):
-    # Ustawienia generatora obrazów
-    datagen = ImageDataGenerator(rescale=1./255)
+def process(image,label):
+    image = tf.cast(image/255. ,tf.float32)
+    return image,label
 
-    # Przygotowanie generatora danych
-    train_generator = datagen.flow_from_directory(
-        data_dir,
-        target_size=target_size,
-        batch_size=batch_size,
-        class_mode='binary',  # Używamy klasyfikacji binarnej (koty vs psy)
-        classes=['cats', 'dogs']
+def load_and_scale_images(data_dir, target_size=(64, 64), batch_size=128):
+    test_ds = tf.keras.utils.image_dataset_from_directory(
+        directory = data_dir,
+        label_mode = 'binary',
+        labels = 'inferred',
+        color_mode='rgb',
+        image_size = target_size,
+        batch_size = batch_size
     )
-
-    return train_generator
-
-# Przykład użycia:
-# data_generator = load_and_scale_images('Data')
-# x_train, y_train = next(data_generator)  # Wczytuje jedną partię danych
+    return test_ds.map(process)
