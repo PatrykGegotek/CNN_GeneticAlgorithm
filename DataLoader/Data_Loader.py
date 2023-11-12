6a0d595028076fd89f21063e240105fd5177b024
Patryk <patryk26g@gmail.com>
2023-11-12 18:54:47 +0100

    Improvements

--- DataLoader/Data_Loader.py
+++ DataLoader/Data_Loader.py
@@ -1,16 +1,18 @@
 import tensorflow as tf
 
-def process(image,label):
-    image = tf.cast(image/255. ,tf.float32)
-    return image,label
+
+def process(image, label):
+    image = tf.cast(image / 255., tf.float32)
+    return image, label
+
 
 def load_and_scale_images(data_dir, target_size=(64, 64), batch_size=128):
     test_ds = tf.keras.utils.image_dataset_from_directory(
-        directory = data_dir,
-        label_mode = 'binary',
-        labels = 'inferred',
+        directory=data_dir,
+        label_mode='binary',
+        labels='inferred',
         color_mode='rgb',
-        image_size = target_size,
-        batch_size = batch_size
+        image_size=target_size,
+        batch_size=batch_size
     )
     return test_ds.map(process)
--- Genetic_Algorithm/Algorithm.py
+++ Genetic_Algorithm/Algorithm.py
@@ -4,6 +4,7 @@ from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from DataLoader.Data_Loader import load_and_scale_images
 import tensorflow as tf
 
+
 # Definicja podstawowej architektury sieci
 def create_cnn():
     model = Sequential()
@@ -14,7 +15,7 @@ def create_cnn():
     model.add(Flatten())
     model.add(Dense(48, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy',metrics=['acc'])
+    model.compile(loss='binary_crossentropy', metrics=['acc'])
     return model
 
 
@@ -26,8 +27,10 @@ def initialize_population(size):
 # Funkcja oceny sieci z użyciem generatora danych
 def evaluate_networks_with_generator(networks, data_generator):
     scores = []
-    for network in networks:
-        scores.append(network.evaluate(data_generator)[1])
+    for i, network in enumerate(networks, start=1):
+        print(f"Evaluating network {i}")
+        score = network.evaluate(data_generator)
+        scores.append(score[0])
     return scores
 
 
@@ -37,7 +40,7 @@ def select_best(networks, scores, num_best):
     scored_networks = list(zip(networks, scores))
 
     # Sortowanie sieci na podstawie ich wyników
-    scored_networks.sort(key=lambda x: x[1], reverse=True)  # Sortujemy malejąco, większy wynik = lepsza sieć
+    scored_networks.sort(key=lambda x: x[1])  # Sortujemy malejąco, mniejszy wynik funkcji loss = lepsza sieć
 
     # Wybór najlepszych sieci
     selected_networks = [network for network, score in scored_networks[:num_best]]
@@ -48,8 +51,9 @@ def select_best(networks, scores, num_best):
 # Mutacja sieci
 def mutate(network, mutation_rate=0.01, mutation_amount=0.1):
     # Iteracja przez każdą warstwę w modelu
-    for layer in network.layers:
-        weights = layer.get_weights()
+    new_network = create_cnn()
+    for old_layer, new_layer in zip(network.layers, new_network.layers):
+        weights = old_layer.get_weights()
         new_weights = []
         for weight in weights:
             # Dodajemy mutację tylko do właściwych wag, nie do biasów
@@ -58,24 +62,27 @@ def mutate(network, mutation_rate=0.01, mutation_amount=0.1):
                 random_mutation = np.random.normal(scale=mutation_amount, size=weight.shape)
                 weight += mutation_mask * random_mutation
             new_weights.append(weight)
-        layer.set_weights(new_weights)
-    return network
+        new_layer.set_weights(new_weights)
+    return new_network
+
 
 # Krzyżowanie dwóch sieci
 def crossover(parent1, parent2):
-    child=create_cnn()
+    child = create_cnn()
     for i in range(len(parent1.layers)):
         if not parent1.layers[i].get_weights():
             continue
         # Maski dla wag właściwych 
-        mask0=np.random.random(np.shape(parent1.layers[i].get_weights()[0])) < 0.5
+        mask0 = np.random.random(np.shape(parent1.layers[i].get_weights()[0])) < 0.5
         mask0_neg = np.logical_not(mask0)
         # Maski dla biasu 
-        mask1=np.random.random(np.shape(parent1.layers[i].get_weights()[1])) < 0.5
+        mask1 = np.random.random(np.shape(parent1.layers[i].get_weights()[1])) < 0.5
         mask1_neg = np.logical_not(mask1)
         # Ustawienie wag
-        child.layers[i].set_weights([mask0*parent1.layers[i].get_weights()[0]+mask0_neg*parent2.layers[i].get_weights()[0], # wagi właściwe
-                                     mask1*parent1.layers[i].get_weights()[1]+mask1_neg*parent2.layers[i].get_weights()[1]]) # bias
+        child.layers[i].set_weights(
+            [mask0 * parent1.layers[i].get_weights()[0] + mask0_neg * parent2.layers[i].get_weights()[0],
+             # wagi właściwe
+             mask1 * parent1.layers[i].get_weights()[1] + mask1_neg * parent2.layers[i].get_weights()[1]])  # bias
     return child
 
 
@@ -83,19 +90,22 @@ def genetic_algorithm(data_generator, generations, population_size, num_best):
     # Inicjalizacja populacji
     population = initialize_population(population_size)
 
-    for generation in range(generations):
+    for i, generation in enumerate(range(generations), start=1):
+        print(f"Generation {i}:")
         # Ocena każdej sieci w populacji na podstawie danych z generatora
         scores = evaluate_networks_with_generator(population, data_generator)
 
         # Wybór najlepszych sieci
         best_networks = select_best(population, scores, num_best)
 
+        print(len(best_networks))
         # Tworzenie nowej populacji
         new_population = []
 
         # Mutacja najlepszych sieci do nowej populacji
         for network in best_networks:
             new_population.append(mutate(network))
+            new_population.append(network)
 
         # Uzupełnienie nowej populacji losowo zmutowanymi sieciami
         while len(new_population) < population_size:
@@ -105,7 +115,7 @@ def genetic_algorithm(data_generator, generations, population_size, num_best):
         # Aktualizacja populacji na nową generację
         population = new_population
 
-        print(f"Generation {generation + 1}/{generations} completed.")
+        print(f"Generation {i} completed.")
 
     # Zwracanie najlepszej sieci z ostatniej generacji
     final_scores = evaluate_networks_with_generator(population, data_generator)
